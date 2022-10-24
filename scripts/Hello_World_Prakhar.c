7ab926e7550331cfa8246b51c0ae38d7c701dadd
prakhar2347 <114171306+prakhar2347@users.noreply.github.com>
2022-10-24 09:00:18 +0530

    Create Hello_World_Prakhar.c

---
+++ scripts/Hello_World_Prakhar.c
@@ -0,0 +1,12 @@
+// Language: C
+// Contributor: Prakhar Rathore
+// Github profile link: (https://github.com/prakhar2347)
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("\nHello World!\n");
+    return 0;
+}
